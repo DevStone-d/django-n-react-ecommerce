6154a5a35df2d82377e6caaac022c735c3575c3b
berk <berkcohadar1@gmail.com>
2020-11-03 20:16:39 +0300

    api endpoints explained

--- devstone/api/products/urls.py
+++ devstone/api/products/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('detail/<int:pk>/', productDetail , name='product detail'),
     path('add/', addProduct.as_view() , name='add product'),
     path('add/detail/<int:pk>/', addProductDetail , name='add product'),
-    #path('detail/<slug>',ProductDetailList.as_view()  ,  name='product'),
     path('media/<slug>',ProductMediaList.as_view()  ,  name='media'),
     path('tag/',ProductsTagList.as_view()  ,  name='tag'),
     path('tag/<slug>',ProductTagList.as_view()  ,  name='tag'),
