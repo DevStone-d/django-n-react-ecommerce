a6376846fb32b29c776bfbe07582748a5ccb879d
Deger <adeger34@gmail.com>
2021-01-12 14:40:02 +0300

    sitemap edit

--- devstone/api/core/urls.py
+++ devstone/api/core/urls.py
@@ -4,7 +4,7 @@ from api.core.views import getList,getFullSiteMap
 
 urlpatterns = [
     path('' ,  getFullSiteMap.as_view()  ,  name='sitemap'),
-    path('collections/<slug>',getList.as_view()  ,  name='deneme'),
+    # path('collections/<slug>',getList.as_view()  ,  name='deneme'),
     # path('add/',addCollection.as_view()  ,  name='addcollection'),
     # path('delete/<slug>/', deleteCollection.as_view() ,  name='deletecollection'),
     # path('update/<slug>/', deleteCollection.as_view() ,  name='deletecollection'),
